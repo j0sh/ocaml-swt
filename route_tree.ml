27d08f4c91098f3074c0aa1d88d1f393f349f8ca
Josh Allmann <joshua.allmann@gmail.com>
2015-01-16 16:44:09 -0800

    swt: Run ocp-indent.

--- route_tree.ml
+++ route_tree.ml
@@ -6,104 +6,104 @@ type 'a query_res = Result of 'a option * (string * string) list
 
 let default x = function
     None -> x
-    | Some y -> y
+  | Some y -> y
 
 let get_key = function
-    | Node(x, _, _) -> x
+  | Node(x, _, _) -> x
 
 let get_value = function
-    | Node(_, x, _) -> x
+  | Node(_, x, _) -> x
 
 let get_branches = function
-    | Node(_, _, x) -> x
+  | Node(_, _, x) -> x
 
 let set_value value node =
-    let Node (key, _, branches) = node in
-    Node(key, value, branches)
+  let Node (key, _, branches) = node in
+  Node(key, value, branches)
 
 
 let find_node elem branches =
-    (* first search for key matches, then wildcards *)
-    (* since the order of the branch list is undefined
-        search sequentially *)
-    let cmp_t node =
-        match get_key node with
-            (* weirder than `elem = y` but fast(er) ? *)
-            | Txt y -> 0 == String.compare elem y
-            | Wild y -> 0 == String.compare elem y in
-    try Some (List.find cmp_t branches) with Not_found -> None
+  (* first search for key matches, then wildcards *)
+  (* since the order of the branch list is undefined
+      search sequentially *)
+  let cmp_t node =
+    match get_key node with
+    (* weirder than `elem = y` but fast(er) ? *)
+    | Txt y -> 0 == String.compare elem y
+    | Wild y -> 0 == String.compare elem y in
+  try Some (List.find cmp_t branches) with Not_found -> None
 
 let new_node elem =
-    let sub s = String.sub s 1 ((String.length s) - 1) in
-    let b = try elem.[0] = ':' with Invalid_argument _ -> false in
-    let inner = if b then Wild (sub elem) else Txt elem in
-    Node (inner, None, [])
+  let sub s = String.sub s 1 ((String.length s) - 1) in
+  let b = try elem.[0] = ':' with Invalid_argument _ -> false in
+  let inner = if b then Wild (sub elem) else Txt elem in
+  Node (inner, None, [])
 
 let replace_branch branch tree =
-    let a = get_key branch in
-    let Node (v, q, branches) = tree in
-    let filt node =
-        let b = get_key node in
-        match a, b with
-            Wild _, Wild _ -> true
-            | Txt c, Txt d -> 0 != String.compare c d
-            | _, _ -> true in
-    let sans_a = List.filter filt branches in
-    Node (v, q, branch::sans_a)
+  let a = get_key branch in
+  let Node (v, q, branches) = tree in
+  let filt node =
+    let b = get_key node in
+    match a, b with
+      Wild _, Wild _ -> true
+    | Txt c, Txt d -> 0 != String.compare c d
+    | _, _ -> true in
+  let sans_a = List.filter filt branches in
+  Node (v, q, branch::sans_a)
 
 let rec build tree parts value =
-    let Node (v, _, branches) = tree in
-    match parts with
+  let Node (v, _, branches) = tree in
+  match parts with
     [] -> tree
-    | h :: [] ->
-        let n = default (new_node h) (find_node h branches) in
-        let q = set_value value n in
-        replace_branch q tree
-    | h::t -> let n = default (new_node h) (find_node h branches) in
-        replace_branch (build n t value) tree
+  | h :: [] ->
+    let n = default (new_node h) (find_node h branches) in
+    let q = set_value value n in
+    replace_branch q tree
+  | h::t -> let n = default (new_node h) (find_node h branches) in
+    replace_branch (build n t value) tree
 
 let add_node str value tree =
-    let parts = split (regexp "/") str in
-    build tree parts (Some value)
+  let parts = split (regexp "/") str in
+  build tree parts (Some value)
 
 let append elem parms node =
-    match get_key node with
+  match get_key node with
     Txt _ -> parms
-    | Wild w -> (w, elem)::parms
+  | Wild w -> (w, elem)::parms
 
 let find_node2 elem branches =
-    (* first search for key matches, then wildcards *)
-    (* since the order of the branch list is undefined
-        search sequentially *)
-    let cmp_t node =
-        match get_key node with
-            (* weirder than `elem = y` but fast(er) ? *)
-            Txt y -> 0 == String.compare elem y
-            | Wild _ -> false in
-    let cmp_w node =
-        match get_key node with
-            Txt _ -> false
-            | Wild _ -> true in
-    (* cmp_t should never return > 1 elements... *)
-    let nodes = List.filter cmp_t branches in
-    if List.length nodes > 0 then nodes else List.filter cmp_w branches
+  (* first search for key matches, then wildcards *)
+  (* since the order of the branch list is undefined
+      search sequentially *)
+  let cmp_t node =
+    match get_key node with
+    (* weirder than `elem = y` but fast(er) ? *)
+      Txt y -> 0 == String.compare elem y
+    | Wild _ -> false in
+  let cmp_w node =
+    match get_key node with
+      Txt _ -> false
+    | Wild _ -> true in
+  (* cmp_t should never return > 1 elements... *)
+  let nodes = List.filter cmp_t branches in
+  if List.length nodes > 0 then nodes else List.filter cmp_w branches
 
 let rec query tree parms = function
     [] -> Some (Result ((get_value tree), List.rev parms))
-    | h::t ->
-        let branches = get_branches tree in
-        let nodes = find_node2 h branches in
-        if 0 = List.length nodes then None else
-        (* now find 'best' match: *)
-        let f n = query n (append h parms n) t in
-        let res = List.map f nodes in
-        (* filter out all empty matches *)
-        let f = function
-            | Some (Result ((Some _), _)) -> true
-            | _ -> false in
-        let res = List.filter f res in
-        try List.hd res with Failure _ -> None
+  | h::t ->
+    let branches = get_branches tree in
+    let nodes = find_node2 h branches in
+    if 0 = List.length nodes then None else
+      (* now find 'best' match: *)
+      let f n = query n (append h parms n) t in
+      let res = List.map f nodes in
+      (* filter out all empty matches *)
+      let f = function
+        | Some (Result ((Some _), _)) -> true
+        | _ -> false in
+      let res = List.filter f res in
+      try List.hd res with Failure _ -> None
 
 let search path tree =
-    let sp = split (regexp "/") path in
-    query tree [] sp
+  let sp = split (regexp "/") path in
+  query tree [] sp
--- server.ml
+++ server.ml
@@ -2,53 +2,53 @@ module Server = Cohttp_lwt_unix.Server
 
 module Env = struct
 
-     type t = {
-        cxnid: Cohttp_lwt_unix.Server.conn;
-        request: Cohttp.Request.t;
-        body: Cohttp_lwt_body.t;
-        mutable params: (string * string) list;
-    } with fields
+  type t = {
+    cxnid: Cohttp_lwt_unix.Server.conn;
+    request: Cohttp.Request.t;
+    body: Cohttp_lwt_body.t;
+    mutable params: (string * string) list;
+  } with fields
 
-    let make ?(params = []) cxnid request body =
-        { cxnid; request; body; params; }
+  let make ?(params = []) cxnid request body =
+    { cxnid; request; body; params; }
 
-    let param env name =
-        let p = params env in
-        let f (k, _) = 0 = String.compare k name in
-        let (_, v)  = List.find f p in v
+  let param env name =
+    let p = params env in
+    let f (k, _) = 0 = String.compare k name in
+    let (_, v)  = List.find f p in v
 
 end
 
 type resp = (Server.Response.t * Cohttp_lwt_body.t) Lwt.t
 
 module Middleware = struct
-    type t = MW of (Env.t -> t -> resp) list
-    let return mw = MW mw
-    let empty = return []
-    let add handler (MW mw) = return (handler :: mw)
-    let create handler = add handler empty
-    let call env = function
-        | MW [] -> Server.respond_not_found ()
-        | MW (h::t) -> h env (return t)
-    let prepare (MW mw) = return (List.rev mw)
-    let chain (MW a) (MW b) = return (b @ a)
+  type t = MW of (Env.t -> t -> resp) list
+  let return mw = MW mw
+  let empty = return []
+  let add handler (MW mw) = return (handler :: mw)
+  let create handler = add handler empty
+  let call env = function
+    | MW [] -> Server.respond_not_found ()
+    | MW (h::t) -> h env (return t)
+  let prepare (MW mw) = return (List.rev mw)
+  let chain (MW a) (MW b) = return (b @ a)
 end
 
 let routes = Array.init 7 (fun _ -> Route_tree.new_node "")
 
 let int_of_meth = function
-    | `GET -> 0
-    | `POST -> 1
-    | `HEAD -> 2
-    | `DELETE -> 3
-    | `PATCH -> 4
-    | `PUT -> 5
-    | `OPTIONS -> 6
-    | `Other _ -> 7
+  | `GET -> 0
+  | `POST -> 1
+  | `HEAD -> 2
+  | `DELETE -> 3
+  | `PATCH -> 4
+  | `PUT -> 5
+  | `OPTIONS -> 6
+  | `Other _ -> 7
 
 let register meth route handler =
-    let m = int_of_meth meth in
-    routes.(m) <- Route_tree.add_node route handler routes.(m)
+  let m = int_of_meth meth in
+  routes.(m) <- Route_tree.add_node route handler routes.(m)
 
 let get = register `GET
 let post = register `POST
@@ -66,36 +66,36 @@ let dispatcher = Middleware.create begin fun env m ->
     let uri = Cohttp.Request.uri req |> Uri.path in
     match Route_tree.search uri table with
     | Some (Route_tree.Result ((Some fn), params)) ->
-        let f (a, b) = (a, Uri.pct_decode b) in
-        List.map f params |> Env.set_params env;
-        fn env
+      let f (a, b) = (a, Uri.pct_decode b) in
+      List.map f params |> Env.set_params env;
+      fn env
     | _ -> begin try
-        (* todo: branch to middleware that properly handles files, dirs, &c *)
-        let uri = Cohttp.Request.uri req in
-        let fname = Server.resolve_local_file ~docroot:"" ~uri in
-        Server.respond_file ~fname ()
+          (* todo: branch to middleware that properly handles files, dirs, &c *)
+          let uri = Cohttp.Request.uri req in
+          let fname = Server.resolve_local_file ~docroot:"" ~uri in
+          Server.respond_file ~fname ()
         with _ -> Server.respond_not_found ()
-    end
- end
+      end
+  end
 
 let exn_handler = Middleware.create begin fun env m ->
     let res = try Middleware.call env m with exn ->
-        let body = Printexc.to_string exn in
-        Server.respond_error ~body () in
+      let body = Printexc.to_string exn in
+      Server.respond_error ~body () in
     flush stdout;
     res
-end
+  end
 
 let make_server middleware =
-    let middleware = Middleware.prepare middleware in
-    let callback conninfo req body =
-        let env = Env.make conninfo req body in
-        Middleware.call env middleware in
-    let conn_closed (ch, conn) = () in
-    let config = Server.make ~callback ~conn_closed () in
-    Server.create config
+  let middleware = Middleware.prepare middleware in
+  let callback conninfo req body =
+    let env = Env.make conninfo req body in
+    Middleware.call env middleware in
+  let conn_closed (ch, conn) = () in
+  let config = Server.make ~callback ~conn_closed () in
+  Server.create config
 
 let run ?(middleware = Middleware.empty) () =
-    let (@@) = Middleware.chain in
-    let middleware = exn_handler @@ middleware @@ dispatcher in
-    make_server middleware |> Lwt_main.run
+  let (@@) = Middleware.chain in
+  let middleware = exn_handler @@ middleware @@ dispatcher in
+  make_server middleware |> Lwt_main.run
