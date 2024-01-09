b618e80bac4cf70d3e9aa2c4a8f99cf99831a750
Josh Allmann <joshua.allmann@gmail.com>
2024-01-09 12:33:13 -0800

    update to dune, remove oasis

---
+++ lib/swt.ml
@@ -0,0 +1,6 @@
+module Auth = Auth
+module Env = Swt_in.Env
+module Middleware = Swt_in.Middleware
+module type Server_intf = Swt_in.Server_intf
+module DefaultServer = Swt_in.DefaultServer
+module MakeServer = Swt_in.MakeServer
